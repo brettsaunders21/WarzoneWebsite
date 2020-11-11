04846ee9f24ff6185394dbef57338b8df0e23dec
Brett Saunders <brett_saunders@btinternet.com>
2020-11-11 22:16:38 +0000

    Added Theme

--- public/app.js
+++ public/app.js
@@ -6,8 +6,6 @@ const whenSignedOut = document.getElementById('whenSignedOut');
 const signInBtn = document.getElementById('signInBtn');
 const signOutBtn = document.getElementById('signOutBtn');
 
-const userDetails = document.getElementById('userDetails');
-
 const provider = new firebase.auth.GoogleAuthProvider();
 
 signInBtn.onclick = () => auth.signInWithPopup(provider);
@@ -18,11 +16,9 @@ auth.onAuthStateChanged(user => {
         // signed in
         whenSignedIn.hidden = false;
         whenSignedOut.hidden = true;
-        userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
     } else {
         // not signed in
         whenSignedIn.hidden = true;
         whenSignedOut.hidden = false;
-        userDetails.innerHTML = '';
     }
 });
