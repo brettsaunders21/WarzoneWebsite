700b6f859dd751f39202e8c99fb4d09dc454650b
Brett Saunders <brett_saunders@btinternet.com>
2020-11-12 20:21:46 +0000

    Added Cod Functions

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
 const functions = require('firebase-functions');
+const firestore = require('firebase-firestore');
+
+var db = firebase.firestore();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +10,13 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+exports.newuser = functions.auth.user().onCreate((user) => {
+    db.collection("users").doc(user.uid).set({
+        displayName: user.displayName,
+        email: user.email,
+        gameIDAdded: false
+    }).catch(function(error) {
+        console.error("Error writing document: ", error);
+    });
+  });
--- public/app.js
+++ public/app.js
@@ -1,4 +1,8 @@
 const auth = firebase.auth();
+const API = require('call-of-duty-api')({ platform: "battle" });
+const firestore = require('firebase-firestore');
+
+var db = firebase.firestore();
 
 const whenSignedIn = document.getElementById('whenSignedIn');
 const whenSignedOut = document.getElementById('whenSignedOut');
@@ -26,3 +30,35 @@ auth.onAuthStateChanged(user => {
         whenSignedOut.hidden = false;
     }
 });
+
+function updateAccount() {
+    var accountName = document.getElementById('calldutyid');
+    var user = firebase.auth().currentUser;
+
+    API.login("brett_saunders@btinternet.com", "").then(data2 => {
+        //I want Warzone Data
+        API.MWcombatwz(accountName).then(data => {
+            data.matches.forEach((value) => {
+                db.collection("users").doc(user.uid).collection("games").doc(value.matchID).set({
+                    draw: value.draw,
+                    duration: value.duration,
+                    map: value.map,
+                    mode: value.mode,
+                    playerCount: value.playerCount,
+                    teamCount: value.teamCount,
+                    rank: value.player.rank,
+                    kills: value.playerStats.kills,
+                    score: value.playerStats.score,
+                    deaths: value.playerStats.deaths,
+                    damageDone: value.playerStats.damageDone,
+                    damageTaken: value.playerStats.damageTaken,
+                    assists: value.playerStats.assists
+                });
+            });
+        }).catch(err => {
+            console.log(err);
+        });
+    }).catch(err => {
+        console.log(err);
+    });
+}
